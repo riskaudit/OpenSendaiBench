c1b3bcf2fb8d95304a8ba9b6942033c2cb354c83
jtdimasaka <jtd33@cam.ac.uk>
2024-01-25 13:31:04 +0800

    Updated constants.py for satellite signals

--- constants.py
+++ constants.py
@@ -1,8 +1,15 @@
 # %%
 import pandas as pd
-data = pd.read_csv('bldgtype_country.csv')
 
+# building type per country
+data = pd.read_csv('bldgtype_country.csv')
 labels = {}
 for i in range(data.shape[0]):
     labels[data['Country'][i]] = list(data.columns[(data == 1).iloc[i]])
+
+# signals per country
+data1 = pd.read_csv('signals_country.csv')
+signals = {}
+for i in range(data1.shape[0]):
+    signals[data1['Country'][i]] = list(data1.columns[(data1 == 1).iloc[i]])
 # %%
